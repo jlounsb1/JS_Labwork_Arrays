59f9f28911ea0920a82a5a8c7a0dff3f367b4cb9
jlounsb1 <jefflounsbury@gmail.com>
2024-05-26 15:18:13 -0400

    manipulated array of objects

--- arrays.js
+++ arrays.js
@@ -1,9 +1,3 @@
-/*
-Declare a variable that stores the number of columns in each row of data within the CSV.
-Instead of hard-coding four columns per row, expand your code to accept any number of columns. This should be calculated dynamically based on the first row of data.
-
-*/
-
 const data ="ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
 let array = data.split(`\n`)
@@ -22,15 +16,10 @@ let newArray4 = array[3].split(',');
 
 let newArray5 = array[4].split(',');
 //console.log(newArray5);
+
 console.log(array)
 combinedArray = [newArray1, newArray2, newArray3, newArray4, newArray5];
 console.log(combinedArray);
-
-
-// For each row of data in the result array produced by your code above, create an object where the key of each value is the heading for that value’s column.
-// Convert these keys to all lowercase letters for consistency.
-// Store these objects in an array, in the order that they were originally listed.
-// Since the heading for each column will be stored in the object keys, you do not need to create an object for the heading row itself.
 
 obj1 = {};
 for(const key in newArray1) {
@@ -42,7 +31,6 @@ console.log(obj1);
 
 obj2 = {};
 for(const key in newArray1) {
-   
     obj2[newArray1[key]] = `${newArray3[key]}`;
 }
 
@@ -50,7 +38,6 @@ console.log(obj2);
 
 obj3 = {};
 for(const key in newArray1) {
-    
     obj3[newArray1[key]] = `${newArray4[key]}`;
 }
 
@@ -58,7 +45,6 @@ console.log(obj3);
 
 obj4 = {};
 for(const key in newArray1) {
-    
     obj4[newArray1[key]] = `${newArray5[key]}`;
 }
 
@@ -66,3 +52,22 @@ console.log(obj4);
 
 arrayObjects =[obj1,obj2,obj3,obj4];
 console.log(arrayObjects);
+
+//remove last element
+arrayObjects.pop();
+
+console.log(arrayObjects);
+//Create new element and add it to the end
+newObj4 = {
+    ID: '7', 
+    Name: 'Bilbo', 
+    Occupation: 'None', 
+    Age: '111'};
+
+    arrayObjects.push(newObj4);
+console.log(arrayObjects);
+
+
+//find the average age using a loop.
+//lower case the keys
+//convert the final set of data back to a srting.
